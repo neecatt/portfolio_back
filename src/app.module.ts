37df4e39e4baf8f780321c860e01a1cad87b0ea4
HorrorOTB <nicatabdullazade135@gmail.com>
2023-03-02 14:10:22 +0400

    Configured uploadmiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
@@ -6,7 +6,8 @@ import { ExperienceModule } from './experience/experience.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
 import { MulterModule } from '@nestjs/platform-express';
-import { DownloadModule } from './download/download.module';
+import { UploadMiddleware } from './upload/upload.middleware';
+import { UploadService } from './upload/upload.service';
 
 @Module({
   imports: [
@@ -19,6 +20,10 @@ import { DownloadModule } from './download/download.module';
     DownloadModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UploadService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer) {
+    consumer.apply(UploadMiddleware).forRoutes('upload');
+  }
+}
--- src/experience/dto/update-experience.dto.ts
+++ src/experience/dto/update-experience.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExperienceDto } from './create-experience.dto';
 
-export class UpdateExperienceDto extends PartialType(CreateExperienceDto) {
-    
-}
+export class UpdateExperienceDto extends PartialType(CreateExperienceDto) {}
