f08ba8b1f632f5d353c9587b0ca7528e9cfd8446
HorrorOTB <nicatabdullazade135@gmail.com>
2024-07-06 03:25:43 +0400

    chore: Add global validation pipe and logger to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:5173',
-  })
+  });
+  const logger = new Logger('Info');
+  logger.log(`Application listening on port 3000`);
   await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
 }
+
 bootstrap();
