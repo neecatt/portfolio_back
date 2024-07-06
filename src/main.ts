5ebf41b45836172f7eea7650be902c53cc867ab6
HorrorOTB <nicatabdullazade135@gmail.com>
2024-07-06 13:44:49 +0400

    chore: Update CORS origin to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
   });
   const logger = new Logger('Info');
   logger.log(`Application listening on port 3000`);
